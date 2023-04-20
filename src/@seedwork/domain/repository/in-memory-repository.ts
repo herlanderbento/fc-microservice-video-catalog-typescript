1a864207c62e24809dd946ee77e3cbba7e7a083a
Herlander Bento <herlanderbento19@gmail.com>
2023-04-20 15:50:46 +0100

    feat: :closed_lock_with_key:

---
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -0,0 +1,56 @@
+import Entity from "../entity/entity";
+import NotFoundError from "../errors/not-found.error";
+import UniqueEntityId from "../value-objects/unique-entity-id.vo";
+import { RepositoryInterface } from "./repository-contracts";
+
+export default abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  public async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  public async findById(id: string | UniqueEntityId): Promise<E> {
+    const _id: string = `${id}`;
+
+    return this.get(_id);
+  }
+
+  public async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  public async update(entity: E): Promise<void> {
+    await this.get(entity.id);
+
+    const indexFound: number = this.items.findIndex(
+      (item: E): boolean => item.id === entity.id
+    );
+
+    this.items[indexFound] = entity;
+  }
+
+  public async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id: string = `${id}`;
+
+    await this.get(_id);
+
+    const indexFound: number = this.items.findIndex(
+      (item: E): boolean => item.id === _id
+    );
+
+    this.items.splice(indexFound, 1);
+  }
+
+  protected async get(id: string): Promise<E> {
+    const item: E = this.items.find((item: E): boolean => item.id === id);
+
+    if (!item) {
+      throw new NotFoundError(`Entity Not Found using ${id}`);
+    }
+
+    return item;
+  }
+}
