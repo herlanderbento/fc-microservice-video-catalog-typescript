247cf12ef6955d102df7c7ac3e39f3309e9e609a
Herlander Bento <herlanderbento19@gmail.com>
2023-04-21 08:20:12 +0100

    test: :test_tube: add repository-contracts.spec in @seedwork domain
    create tests repository contracts
    
    Ref issue #2

--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -9,10 +9,101 @@ export interface RepositoryInterface<E extends Entity> {
   delete(id: string | UniqueEntityId): Promise<void>;
 }
 
+export type SortDirection = "asc" | "desc";
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  sort_dir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams<Filter = string> {
+  protected _page: number;
+  protected _per_page: number = 15;
+  protected _sort: string | null;
+  protected _sort_dir: SortDirection | null;
+  protected _filter: Filter | null;
+
+  public constructor(props: SearchProps<Filter> = {}) {
+    this.page = props.page;
+    this.per_page = props.per_page;
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+  }
+
+  public get page() {
+    return this._page;
+  }
+
+  private set page(value: number) {
+    let _page = +value;
+
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1;
+    }
+
+    this._page = _page;
+  }
+
+  public get per_page() {
+    
+    return this._per_page;
+  }
+
+  private set per_page(value: number) {
+    let _per_page = value === (true as any) ? this._per_page : +value;
+
+    if (
+      Number.isNaN(_per_page) ||
+      _per_page <= 0 ||
+      parseInt(_per_page as any) !== _per_page
+    ) {
+      _per_page = this._per_page;
+    }
+
+    this._per_page = _per_page;
+  }
+
+  public get sort(): string | null {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === "" ? null : `${value}`;
+  }
+
+  public get sort_dir(): SortDirection | null {
+    return this._sort_dir;
+  }
+
+  private set sort_dir(value: string | null) {
+    if (!this.sort) {
+      this._sort_dir = null;
+      return;
+    }
+    const dir = `${value}`.toLowerCase();
+    this._sort_dir = dir !== "asc" && dir !== "desc" ? "asc" : dir;
+  }
+
+  public get filter(): Filter | null {
+    return this._filter;
+  }
+
+  protected set filter(value: Filter | null) {
+    this._filter =
+      value === null || value === undefined || (value as unknown) === ""
+        ? null
+        : (`${value}` as any);
+  }
+}
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchParams,
-  SearchResult
+  SearchOutput,
+  SearchInput = SearchParams
 > extends RepositoryInterface<E> {
-  search(searchParams: SearchParams): Promise<SearchResult>;
+  search(searchParams: SearchInput): Promise<SearchOutput>;
 }
