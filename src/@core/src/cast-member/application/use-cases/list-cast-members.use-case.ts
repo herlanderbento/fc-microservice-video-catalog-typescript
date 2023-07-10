40f9d4aac4748394be516c7a2ec9d7836512d188
herlanderbento <herlanderbento@19gmail.com>
2023-07-10 15:35:25 +0100

    feat: :sparkles: add new feature for cast-member, list-cast-member.use-case and list-cast-member.use-case.spec
    create new feature for cast-member, list-cast-member.use-case and list-cast-member.use-case.spec
    
    Ref issue #5

---
+++ src/@core/src/cast-member/application/use-cases/list-cast-members.use-case.ts
@@ -0,0 +1,35 @@
+import { Types } from "../../domain/value-object/cast-member-type.vo";
+import { default as DefaultUseCase } from "../../../@seedwork/application/use-cases";
+import { CastMemberOutput, CastMemberOutputMapper } from "../dto";
+import { SearchInputDto } from "../../../@seedwork/application/dto/search-input";
+import {
+  PaginationOutputDto,
+  PaginationOutputMapper,
+} from "../../../@seedwork/application/dto/pagination-output";
+import { CastMemberRepository } from "../../domain";
+
+export namespace ListCastMembersUseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private castMemberRepository: CastMemberRepository.Repository
+    ) {}
+
+    public async execute(input: Input): Promise<Output> {
+      const params = CastMemberRepository.SearchParams.create(input);
+      const searchResult = await this.castMemberRepository.search(params);
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: CastMemberRepository.SearchResult): Output {
+      const items = searchResult.items.map((item) => {
+        return CastMemberOutputMapper.toOutput(item);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
+    }
+  }
+
+  export type Input = SearchInputDto<{ name?: string; type?: Types }>;
+
+  export type Output = PaginationOutputDto<CastMemberOutput>;
+}
+export default ListCastMembersUseCase;
